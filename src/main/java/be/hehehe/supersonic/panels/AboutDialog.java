d87b0c18de4d1b1bd06a5f32c61d8af8fdd032f3
Athou <jeremiepanzer@gmail.com>
2012-02-09 15:35:19 +0100

    about dialog redesign

--- src/main/java/be/hehehe/supersonic/panels/AboutDialog.java
+++ src/main/java/be/hehehe/supersonic/panels/AboutDialog.java
@@ -1,17 +1,25 @@
 package be.hehehe.supersonic.panels;
 
 import java.awt.Container;
+import java.awt.Cursor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URI;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import javax.swing.Action;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 import net.miginfocom.swing.MigLayout;
+
+import org.jdesktop.swingx.hyperlink.HyperlinkAction;
+
 import be.hehehe.supersonic.service.IconService;
 import be.hehehe.supersonic.service.VersionService;
 import be.hehehe.supersonic.utils.SwingUtils;
@@ -34,11 +42,22 @@ public class AboutDialog extends JDialog {
 
 		Container panel = getContentPane();
 
-		panel.setLayout(new MigLayout("", "[]", "[]"));
-		panel.add(
-				new JLabel("Supersonic version " + versionService.getVersion()),
-				"cell 0 0");
-		panel.add(new JLabel("by Athou"), "cell 0 1");
+		panel.setLayout(new MigLayout("", "[][]", "[]"));
+
+		panel.add(new JLabel("Supersonic"), "cell 0 0");
+		panel.add(new JLabel("v" + versionService.getVersion()), "cell 1 0");
+
+		panel.add(new JLabel("Author"), "cell 0 1");
+		panel.add(new JLabel("Athou"), "cell 1 1");
+
+		panel.add(new JLabel("Download and Sources"), "cell 0 2");
+		panel.add(new Hyperlink("GitHub",
+				"https://github.com/Athou/Supersonic/downloads"), "cell 1 2");
+
+		panel.add(new JLabel("Discussion"), "cell 0 3");
+		panel.add(new Hyperlink("Subsonic Forum",
+				"http://forum.subsonic.org/forum/viewtopic.php?f=4&t=8823"),
+				"cell 1 3");
 
 		JButton okButton = new JButton("OK");
 		okButton.addActionListener(new ActionListener() {
@@ -47,9 +66,25 @@ public class AboutDialog extends JDialog {
 				dispose();
 			}
 		});
-		panel.add(okButton, "cell 0 3, center");
+		panel.add(okButton, "cell 0 4 2 1, center");
 
 		pack();
 		SwingUtils.centerContainer(this);
+	}
+
+	private class Hyperlink extends JButton {
+		public Hyperlink(String label, String uri) {
+			super();
+			setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+			Action action = HyperlinkAction.createHyperlinkAction(URI
+					.create(uri));
+			action.putValue(Action.NAME, "<html><u>" + label + "</u></html>");
+			setAction(action);
+			setHorizontalAlignment(SwingConstants.LEFT);
+			setBorder(BorderFactory.createEmptyBorder());
+			setBorderPainted(false);
+			setContentAreaFilled(false);
+			setFocusable(false);
+		}
 	}
 }
