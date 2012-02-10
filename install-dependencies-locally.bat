0ca4edc34ec9f6bb3ba9ffb47b5e25abc6b9b46d
Athou <jeremiepanzer@gmail.com>
2012-02-10 09:19:36 +0100

    moved dependencies to a maven repo

--- install-dependencies-locally.bat
+++
@@ -1,2 +0,0 @@
-call mvn org.apache.maven.plugins:maven-install-plugin:2.3.1:install-file -Dfile=lib/mp3spi1.9.5.jar -DgroupId=org.mobicents.external.fmj -DartifactId=mp3spi -Dversion=1.9.5 -Dpackaging=jar
-call mvn org.apache.maven.plugins:maven-install-plugin:2.3.1:install-file -Dfile=lib/jaad-0.8.3.jar -DgroupId=net.sourceforge.jaadec -DartifactId=jaad -Dversion=0.8.3 -Dpackaging=jar
